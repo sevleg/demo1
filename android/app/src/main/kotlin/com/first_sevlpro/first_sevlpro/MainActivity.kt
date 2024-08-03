2f18cf1a82b005d515385b53254eef7ef8d78975
sevleg <egunovsevl@gmail.com>
2024-08-03 17:46:18 +0300

    Init commit My first project on flutter

---
+++ android/app/src/main/kotlin/com/first_sevlpro/first_sevlpro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.first_sevlpro.first_sevlpro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyFirstApp());
+}
+
+class MyFirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: Text("SeVl App"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                LinearProgressIndicator(value: 23),
+                Text(
+                  '23%',
+                  style: TextStyle(color: Colors.green, fontSize: 20),
+                ),
+                Text(
+                  'Press button to download',
+                  style: TextStyle(color: Colors.green, fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.cloud_download),
+        ),
+      ),
+    );
+  }
+}
